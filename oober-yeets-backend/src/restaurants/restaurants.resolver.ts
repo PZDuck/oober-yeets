c0cd6f74d02f5848db0df1ce1d9a1ffac0e11540
PZDuck <agp2281337@gmail.com>
2021-03-15 23:23:29 -0400

    initial setup, connected postgres database, created mock entity for testing purposes

---
+++ oober-yeets-backend/src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,17 @@
+import { Query, Resolver, Mutation, Args } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
+
+@Resolver(() => Restaurant)
+export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
+  @Query(() => [Restaurant])
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
+  }
+  @Mutation(() => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    return true;
+  }
+}
